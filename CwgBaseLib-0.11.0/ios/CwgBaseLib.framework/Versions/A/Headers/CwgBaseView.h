543d16b8077ca0030324d04159fca2f40a6ba99e
changwuguo <changwuguo@bytedance.com>
2021-02-23 16:55:33 +0800

    增加第三方SDK

---
+++ CwgBaseLib-0.11.0/ios/CwgBaseLib.framework/Versions/A/Headers/CwgBaseView.h
@@ -0,0 +1,18 @@
+//
+//  CwgBaseView.h
+//  CwgBaseLib
+//
+//  Created by changwuguo on 2021/2/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CwgBaseView : UIView
+
+- (void)createBaseView;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- CwgBaseLib/Classes/CwgBaseView.m
+++ CwgBaseLib/Classes/CwgBaseView.m
@@ -7,10 +7,10 @@
 
 #import "CwgBaseView.h"
 #import <Masonry/Masonry.h>
+#import <JhtMarquee/JhtVerticalMarquee.h>
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface CwgBaseView ()
-
-@property (nonatomic, strong) UIView *aView;
 
 @end
 
@@ -18,14 +18,31 @@
 
 - (void)createBaseView
 {
-    self.aView = [UIView new];
-    self.aView.backgroundColor = [UIColor greenColor];
-    [self addSubview: self.aView];
-    
-    [self.aView mas_makeConstraints:^(MASConstraintMaker *make) {
+    UIView *aView = [UIView new];
+    aView.backgroundColor = [UIColor greenColor];
+    [self addSubview: aView];
+    [aView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.center.mas_equalTo(self);
         make.size.mas_equalTo(CGSizeMake(80, 80));
     }];
+
+    UIImageView *imageView = [UIImageView new];
+    imageView.clipsToBounds = YES;
+    imageView.contentMode = UIViewContentModeScaleAspectFill;
+    [imageView sd_setImageWithURL: [NSURL URLWithString: @"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2340886709,253774581&fm=26&gp=0.jpg"]];
+    [aView addSubview: imageView];
+    [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.mas_equalTo(aView);
+        make.size.mas_equalTo(CGSizeMake(50, 50));
+    }];
+
+    JhtVerticalMarquee *verticalTextCycle = [JhtVerticalMarquee new];
+    verticalTextCycle.numberOfLines = 1;
+    verticalTextCycle.scrollDelay = 5.0f;
+    verticalTextCycle.scrollDuration = 1.0f;
+    verticalTextCycle.textColor = [UIColor blackColor];
+    verticalTextCycle.textAlignment = NSTextAlignmentLeft;
+    verticalTextCycle.textFont = [UIFont systemFontOfSize: 12.0f];
 }
 
 @end
