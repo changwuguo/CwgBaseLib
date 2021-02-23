56eaa8a4e8ccf2732c8f4979522390f7dbcdd75c
changwuguo <changwuguo@bytedance.com>
2021-02-23 17:59:44 +0800

    添加JhtMarquee

--- CwgBaseLib.podspec
+++ CwgBaseLib.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'CwgBaseLib'
-  s.version          = '0.16.0'
+  s.version          = '0.17.0'
   s.summary          = '这是一个简单的介绍'
 
 # This description is used to generate tags and improve search results.
@@ -39,9 +39,19 @@ Pod::Spec.new do |s|
 
   s.public_header_files = 'CwgBaseLib/Classes/**/*.h'
   s.frameworks = 'UIKit', 'Foundation'
-  s.dependency 'Masonry'
-  # s.dependency 'JhtMarquee'
+
   s.dependency 'MLLabel'
+  s.dependency 'Masonry'
+  s.dependency 'JhtMarquee'
+
   # s.pod_target_xcconfig = { 'OTHER_LDFLAGS' => '-ObjC -all_load', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64' }
+  s.pod_target_xcconfig = {
+       'GCC_PRECOMPILE_PREFIX_HEADER' => "NO",
+       'DEBUG_INFORMATION_FORMAT' => "DWARF with dSYM",
+       'CLANG_ENABLE_MODULES' => "NO",
+       'GCC_GENERATE_DEBUGGING_SYMBOLS' => "NO",
+       'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64',
+       'OTHER_CFLAGS' => '-fembed-bitcode'
+     }
   s.user_target_xcconfig = { 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64' }
 end
--- CwgBaseLib/Classes/CwgBaseView.m
+++ CwgBaseLib/Classes/CwgBaseView.m
@@ -7,8 +7,8 @@
 
 #import "CwgBaseView.h"
 #import <Masonry/Masonry.h>
-//#import <JhtMarquee/JhtVerticalMarquee.h>
 #import <MLLabel/MLLabel.h>
+#import <JhtMarquee/JhtVerticalMarquee.h>
 
 @interface CwgBaseView ()
 
@@ -35,23 +35,13 @@
         make.center.mas_equalTo(self);
     }];
 
-//    UIImageView *imageView = [UIImageView new];
-//    imageView.clipsToBounds = YES;
-//    imageView.contentMode = UIViewContentModeScaleAspectFill;
-//    [imageView sd_setImageWithURL: [NSURL URLWithString: @"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2340886709,253774581&fm=26&gp=0.jpg"]];
-//    [aView addSubview: imageView];
-//    [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.center.mas_equalTo(aView);
-//        make.size.mas_equalTo(CGSizeMake(50, 50));
-//    }];
-
-//    JhtVerticalMarquee *verticalTextCycle = [JhtVerticalMarquee new];
-//    verticalTextCycle.numberOfLines = 1;
-//    verticalTextCycle.scrollDelay = 5.0f;
-//    verticalTextCycle.scrollDuration = 1.0f;
-//    verticalTextCycle.textColor = [UIColor blackColor];
-//    verticalTextCycle.textAlignment = NSTextAlignmentLeft;
-//    verticalTextCycle.textFont = [UIFont systemFontOfSize: 12.0f];
+    JhtVerticalMarquee *verticalTextCycle = [JhtVerticalMarquee new];
+    verticalTextCycle.numberOfLines = 1;
+    verticalTextCycle.scrollDelay = 5.0f;
+    verticalTextCycle.scrollDuration = 1.0f;
+    verticalTextCycle.textColor = [UIColor blackColor];
+    verticalTextCycle.textAlignment = NSTextAlignmentLeft;
+    verticalTextCycle.textFont = [UIFont systemFontOfSize: 12.0f];
 }
 
 @end
