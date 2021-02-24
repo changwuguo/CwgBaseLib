7ed9f7265a502bcda90f9d8877ae5536d3bf58f8
changwuguo <changwuguo@bytedance.com>
2021-02-24 20:16:38 +0800

    删除代码

--- CwgBaseLib/Classes/CwgBaseView.m
+++ CwgBaseLib/Classes/CwgBaseView.m
@@ -6,10 +6,10 @@
 //
 
 #import "CwgBaseView.h"
-#import <Masonry/Masonry.h>
+//#import <Masonry/Masonry.h>
 #import <MLLabel/MLLabel.h>
 //#import <JhtMarquee/JhtVerticalMarquee.h>
-#import <SDWebImage/UIImageView+WebCache.h>
+//#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface CwgBaseView ()
 
@@ -19,31 +19,32 @@
 
 - (void)createBaseView
 {
-    UIView *aView = [UIView new];
-    aView.backgroundColor = [UIColor greenColor];
-    [self addSubview: aView];
-    [aView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.center.mas_equalTo(self);
-        make.size.mas_equalTo(CGSizeMake(80, 80));
-    }];
-
-    UIImageView *imageView = [UIImageView new];
-    [aView addSubview: imageView];
-    [imageView sd_setImageWithURL: [NSURL URLWithString: @"https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3118529426,2885674921&fm=26&gp=0.jpg"]];
-    [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.center.mas_equalTo(aView);
-        make.size.mas_equalTo(CGSizeMake(60, 60));
-    }];
+//    UIView *aView = [UIView new];
+//    aView.backgroundColor = [UIColor greenColor];
+//    [self addSubview: aView];
+//    [aView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.center.mas_equalTo(self);
+//        make.size.mas_equalTo(CGSizeMake(80, 80));
+//    }];
+//
+//    UIImageView *imageView = [UIImageView new];
+//    [aView addSubview: imageView];
+//    [imageView sd_setImageWithURL: [NSURL URLWithString: @"https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3118529426,2885674921&fm=26&gp=0.jpg"]];
+//    [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.center.mas_equalTo(aView);
+//        make.size.mas_equalTo(CGSizeMake(60, 60));
+//    }];
 
 
     MLLabel *label = [MLLabel new];
     label.text = @"字节跳动";
     label.font = [UIFont systemFontOfSize: 10.0];
     label.textColor = [UIColor blackColor];
-    [aView addSubview: label];
-    [label mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.center.mas_equalTo(self);
-    }];
+    label.frame = self.bounds;
+    [self addSubview: label];
+//    [label mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.center.mas_equalTo(self);
+//    }];
 
 //    JhtVerticalMarquee *verticalTextCycle = [JhtVerticalMarquee new];
 //    verticalTextCycle.numberOfLines = 1;
