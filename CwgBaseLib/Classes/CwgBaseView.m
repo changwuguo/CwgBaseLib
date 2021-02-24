d833b2a3cc8375808e9ca2c29c1a07ad61b457c3
changwuguo <changwuguo@bytedance.com>
2021-02-24 19:55:27 +0800

    更新代码

--- CwgBaseLib/Classes/CwgBaseView.m
+++ CwgBaseLib/Classes/CwgBaseView.m
@@ -8,7 +8,7 @@
 #import "CwgBaseView.h"
 #import <Masonry/Masonry.h>
 #import <MLLabel/MLLabel.h>
-#import <JhtMarquee/JhtVerticalMarquee.h>
+//#import <JhtMarquee/JhtVerticalMarquee.h>
 #import <SDWebImage/UIImageView+WebCache.h>
 
 @interface CwgBaseView ()
@@ -45,13 +45,13 @@
         make.center.mas_equalTo(self);
     }];
 
-    JhtVerticalMarquee *verticalTextCycle = [JhtVerticalMarquee new];
-    verticalTextCycle.numberOfLines = 1;
-    verticalTextCycle.scrollDelay = 5.0f;
-    verticalTextCycle.scrollDuration = 1.0f;
-    verticalTextCycle.textColor = [UIColor blackColor];
-    verticalTextCycle.textAlignment = NSTextAlignmentLeft;
-    verticalTextCycle.textFont = [UIFont systemFontOfSize: 12.0f];
+//    JhtVerticalMarquee *verticalTextCycle = [JhtVerticalMarquee new];
+//    verticalTextCycle.numberOfLines = 1;
+//    verticalTextCycle.scrollDelay = 5.0f;
+//    verticalTextCycle.scrollDuration = 1.0f;
+//    verticalTextCycle.textColor = [UIColor blackColor];
+//    verticalTextCycle.textAlignment = NSTextAlignmentLeft;
+//    verticalTextCycle.textFont = [UIFont systemFontOfSize: 12.0f];
 }
 
 @end
