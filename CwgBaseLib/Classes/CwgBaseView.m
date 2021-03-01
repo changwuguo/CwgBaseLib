eaa0ad1eadbaaef1fa91301c673f60985538f200
changwuguo <changwuguo@bytedance.com>
2021-03-01 11:49:17 +0800

    删除多余的SDK

--- CwgBaseLib/Classes/CwgBaseView.m
+++ CwgBaseLib/Classes/CwgBaseView.m
@@ -8,8 +8,6 @@
 #import "CwgBaseView.h"
 #import <Masonry/Masonry.h>
 #import <MLLabel/MLLabel.h>
-#import <JhtMarquee/JhtVerticalMarquee.h>
-#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface CwgBaseView ()
 
@@ -29,7 +27,7 @@
 
     UIImageView *imageView = [UIImageView new];
     [aView addSubview: imageView];
-    [imageView sd_setImageWithURL: [NSURL URLWithString: @"https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3118529426,2885674921&fm=26&gp=0.jpg"]];
+    // [imageView sd_setImageWithURL: [NSURL URLWithString: @"https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3118529426,2885674921&fm=26&gp=0.jpg"]];
     [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.center.mas_equalTo(aView);
         make.size.mas_equalTo(CGSizeMake(60, 60));
@@ -45,14 +43,6 @@
     [label mas_makeConstraints:^(MASConstraintMaker *make) {
         make.center.mas_equalTo(self);
     }];
-
-    JhtVerticalMarquee *verticalTextCycle = [JhtVerticalMarquee new];
-    verticalTextCycle.numberOfLines = 1;
-    verticalTextCycle.scrollDelay = 5.0f;
-    verticalTextCycle.scrollDuration = 1.0f;
-    verticalTextCycle.textColor = [UIColor blackColor];
-    verticalTextCycle.textAlignment = NSTextAlignmentLeft;
-    verticalTextCycle.textFont = [UIFont systemFontOfSize: 12.0f];
 }
 
 @end
